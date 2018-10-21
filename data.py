a5180d5d176b675c7edbfe752829f96aec6397b6
V_Robin <valentinrobin1@gmail.com>
2018-10-21 23:51:52 +0200

    added 'plastic_tube' item into the game

---
+++ data.py
@@ -0,0 +1,36 @@
+#The labyrinth structure:
+#W = Wall tile
+#F = Floor tile
+
+level = [
+"WWWWWWWWWWWWWWW",
+"WFWFFFWFFFFFFFW",
+"WFWWWFWFWFWWWFW",
+"WFWFWFWFWFFFWFW",
+"WFFFFFFFWFWWWFW",
+"WWWFWWWFWFFFWFW",
+"WFWFWFWWWFWWWFW",
+"WFFFFFFWFFWFFFW",
+"WWWWFWWWFWWWWFW",
+"WFFFFWFFFFFFWFW",
+"WWFWWWWWFWWWWWW",
+"WFFWFFFFFWFFFFW",
+"WWWWWWFWFWFWWFW",
+"WFFFFFFWFFFFWFW",
+"WWWWWWWWWWWWWWW"
+]
+
+#0, 0 being the top left corner and 14, 14 the bottom right
+#each item should be put in a dead end, you should modifie this list
+#if you modifie the labyrinth above
+possible_item_position = [
+(1 * 40, 6 * 40), (1 * 40, 9 * 40), (1 * 40, 11 * 40), (1 * 40, 13 * 40),
+(3 * 40, 1 * 40), (3 * 40, 3 * 40), (4 * 40, 11 * 40), (5 * 40, 6 * 40),
+(6 * 40, 7 * 40), (6 * 40, 9 * 40), (7 * 40, 5 * 40), (11 * 40, 3 * 40),
+(11 * 40, 5 * 40), (11 * 40, 7 * 40), (11 * 40, 9 * 40), (11 * 40, 13 * 40),
+(13 * 40, 9 * 40)
+]
+
+#a tile is 40 pixels wide so the cordinate for each sprite is multiplied by 40
+MacGyver_starting_position = (1 * 40, 1 * 40) 
+guardian_position = (13 * 40, 13 * 40)
---
+++ main.py
@@ -0,0 +1,51 @@
+import pygame
+from func import *
+from data import *
+from pygame.locals import *
+
+keep_playing = 1
+
+pygame.init()
+pygame.time.Clock().tick(30)
+
+create_labyrinth(level)
+player_position, player = create_player()
+ether_position, needle_position, syringe_position, plastic_tube_position =\
+    item_placement()
+old_player_position = player_position
+
+pygame.key.set_repeat(400, 30)
+while keep_playing:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            keep_playing = 0
+        if event.type == KEYDOWN:
+            if event.key == K_DOWN:
+                if level[player_position[1] // 40 + 1] \
+                        [player_position[0] // 40] != "W":
+                    player_position = player_position.move(0, 40)
+                    keep_playing = victory_condition(ether_position, \
+                        needle_position, syringe_position, plastic_tube_position, player_position)
+            if event.key == K_UP:
+                if level[player_position[1] // 40 - 1] \
+                        [player_position[0] // 40] != "W":
+                    player_position = player_position.move(0, -40)
+                    keep_playing = victory_condition(ether_position, \
+                        needle_position, syringe_position, plastic_tube_position, player_position)
+            if event.key == K_RIGHT:
+                if level[player_position[1] // 40] \
+                        [player_position[0] // 40 + 1] != "W":
+                    player_position = player_position.move(40, 0)
+                    keep_playing = victory_condition(ether_position, \
+                        needle_position, syringe_position, plastic_tube_position, player_position)
+            if event.key == K_LEFT:
+                if level[player_position[1] // 40] \
+                        [player_position[0] // 40 - 1] != "W":
+                    player_position = player_position.move(-40, 0)
+                    keep_playing = victory_condition(ether_position, \
+                        needle_position, syringe_position, plastic_tube_position, player_position)
+
+    window.blit(floor, old_player_position)
+    old_player_position = player_position
+    window.blit(player, player_position)
+    pygame.display.flip()
