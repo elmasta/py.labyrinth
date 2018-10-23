fe21dce7454e50625194b5e5d4e2f594dcb2b3d9
V_Robin <valentinrobin1@gmail.com>
2018-10-24 01:30:38 +0200

    item placement, player gen and display added but not fonctional yet

--- classes.py
+++ classes.py
@@ -10,19 +10,24 @@ window = pygame.display.set_mode((600, 600))
 
 class Generation:
 
-    def display(self):
-        """Méthode permettant d'afficher le niveau en fonction 
-        de la liste de structure renvoyée par generer()"""
-        #Chargement des images (seule celle d'arrivée contient de la transparence)
+    def __init__(self):
+        self.layout = LEVEL
+        self.ether_position = possible_item_position.pop(randrange(17))
+        self.needle_position = possible_item_position.pop(randrange(16))
+        self.syringe_position = possible_item_position.pop(randrange(15))
+        self.plastic_tube_position = possible_item_position.pop(randrange(14))
 
+    def Display(self):
+        """Méthode permettant d'afficher le niveau en fonction
+        de la liste de structure renvoyée par generer()"""
+        
         wall = pygame.image.load("sprites/walls.png").convert()
         guardian = pygame.image.load("sprites/Gardien.png").convert()
         guardian.set_colorkey((255, 0, 255))
-        #On parcourt la liste du niveau
         x = 0
         string = 0
         while string != 15:
-            for element in LEVEL[string]:
+            for element in self.layout[string]:
                 if element is "W":
                     window.blit(wall, (x * 40, string * 40))
                 else:
@@ -32,3 +37,38 @@ class Generation:
             x = 0
         window.blit(guardian, guardian_position)
         pygame.display.flip()
+
+    def ItemPlacement(self):
+
+        ether = pygame.image.load("sprites/ether.png").convert()
+        needle = pygame.image.load("sprites/aiguille.png").convert()
+        syringe = pygame.image.load("sprites/seringue.png").convert()
+        plastic_tube = pygame.image.load("sprites/tube_plastique.png").convert()
+        ether.set_colorkey((255, 0, 255))
+        needle.set_colorkey((255, 0, 255))
+        syringe.set_colorkey((255, 0, 255))
+        plastic_tube.set_colorkey((255, 0, 255))   
+        ether_position = possible_item_position.pop(randrange(17))
+        needle_position = possible_item_position.pop(randrange(16))
+        syringe_position = possible_item_position.pop(randrange(15))
+        plastic_tube_position = possible_item_position.pop(randrange(14))
+        window.blit(ether, self.ether_position)
+        window.blit(needle, self.needle_position)
+        window.blit(syringe, self.syringe_position)
+        window.blit(plastic_tube, self.plastic_tube_position)
+
+class Player:
+
+    def PlayerGeneration(self):
+        player = pygame.image.load("sprites/MacGyver.png").convert()
+        player.set_colorkey((255, 0, 255))
+        player_position = player.get_rect()
+        player_position = player_position.move(MacGyver_starting_position)
+        window.blit(player, MacGyver_starting_position)
+        pygame.display.flip()
+        return player_position, player
+
+
+    def PlayerMovement(self):
+
+    def VictoryCondition(self):
--- main2.py
+++ main2.py
@@ -5,3 +5,7 @@ import pygame
 from classes import *
 from data import *
 from pygame.locals import *
+
+labyrinth_generation = Generation()
+labyrinth_generation.display
+labyrinth_generation.item_placement
